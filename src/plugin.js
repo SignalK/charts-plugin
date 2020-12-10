4abb1ffd5a0ec7d29310a3bf69fc2125a88c79bd
AdrianP <38519157+panaaj@users.noreply.github.com>
2020-12-10 17:13:41 +1030

    add `type` and `chartLayers` selections

--- src/plugin.js
+++ src/plugin.js
@@ -128,6 +128,12 @@ module.exports = function(app) {
                 minimum: MIN_ZOOM,
                 default: 15,
               },
+              serverType: {
+                type: 'string',
+                title: 'Server Type',
+                default: 'tilelayer',
+                enum: ['tilelayer', 'WMS']
+              },
               format: {
                 type: 'string',
                 title: 'Format',
@@ -138,6 +144,15 @@ module.exports = function(app) {
                 type: 'string',
                 title: 'URL',
                 description: 'Tileset URL containing {z}, {x} and {y} parameters, for example "http://example.org/{z}/{x}/{y}.png"'
+              },
+              layers: {
+                type: 'array',
+                title: 'Layers',
+                items: {
+                    title: 'Layer Name',
+                    description: '(WMS maps only) Name of layer to fetch and display',
+                    type: 'string'
+                }
               }
             }
           }
@@ -170,10 +185,11 @@ function convertOnlineProviderConfig(provider) {
     minzoom: Math.min(Math.max(1, provider.minzoom), 19),
     maxzoom: Math.min(Math.max(1, provider.maxzoom), 19),
     format: provider.format,
-    type: 'tilelayer',
     scale: 'N/A',
     identifier: id,
-    tilemapUrl: provider.url
+    tilemapUrl: provider.url,
+    type: (provider.serverType) ? provider.serverType : 'tilelayer',
+    chartLayers: (provider.layers) ? provider.layers : null
   }
 }
 
