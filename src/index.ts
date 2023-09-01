e0bb96b7905af9fb06067d6664a9ad50bec7c327
panaaj <38519157+panaaj@users.noreply.github.com>
2023-09-01 16:56:35 +0930

    test getExternalPort is function

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ module.exports = (app: ChartProviderApp): Plugin => {
     pluginStarted === false && registerRoutes()
     pluginStarted = true
     const urlBase = `${app.config.ssl ? 'https' : 'http'}://localhost:${
-      app.config?.getExternalPort() || 3000
+      'getExternalPort' in app.config ? app.config.getExternalPort() : 3000
     }`
     app.debug('**urlBase**', urlBase)
     app.setPluginStatus('Started')
